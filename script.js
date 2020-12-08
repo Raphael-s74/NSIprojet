302f625f4d3ab9f502ef60a4e9744d1760f81367
Raphael-s74 <75307056+Raphael-s74@users.noreply.github.com>
2020-12-08 11:16:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function anglais() {
-	document.querySelector("#titre1").innerHTML = "The story of the vidéo game"
-	document.querySelector("#presentation").innerHTML = "We are going to introduce you to the incredible history of video games. We will start from the first video games created in the world,through the appearance of consoles and therefore of some companies today known worldwide for arrived at today."
-	document.querySelector("#petitTitre1").innerHTML = "What is a vidéo game ?"
-	document.querySelector("#suite1").innerHTML = "According to a video game is an electronic game with a user interface allowing playful human interaction by generating visual feedback on a video device. The video game player has peripherals to act on the game and perceive the consequences of its actions on a virtual environment. The word 'video' in video games traditionally makes reference to a raster display device, but, following the popularization of the term, it now involves any type of display device"
-	document.querySelector("#lien1").innerHTML = "Wikipedia" MArche pas le lien disparait
-}
